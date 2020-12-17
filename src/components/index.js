bae45ff83fa9f18b17c0f6ef149a972d1f00b104
NielsMoens <nielmoen@student.arteveldehs.be>
2020-12-17 22:35:26 +0100

    learning/added some routing[fix]

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,6 @@
 
 import HomeComponent from './Home';
 import ProductsComponent from './Products';
-// eslint-disable-next-line import/no-unresolved
 import ProductComponent from './Product';
 
 export { HomeComponent, ProductsComponent, ProductComponent };
