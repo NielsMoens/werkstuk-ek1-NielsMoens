1f75411becc94f561620fb057ab1d33e63d2fa27
NielsMoens <nielmoen@student.arteveldehs.be>
2021-01-05 21:34:19 +0100

    added business pins on mapbox map

--- src/components/visitorDashboard.js
+++ src/components/visitorDashboard.js
@@ -7,6 +7,7 @@ import Component from '../lib/components';
 import DataBaseManager from '../lib/DatabaseManager';
 import Elements from '../lib/Elements';
 import userdata from '../lib/userdata';
+import BusinessNames from '../lib/BusinessNames';
 
 class VisitorDashboard extends Component {
   constructor() {
@@ -90,8 +91,9 @@ class VisitorDashboard extends Component {
     document.getElementById('map').className = 'showMap';
 
     // CDN's are injected on the window by default since you import the script in the main index
+    const accesstoken = 'pk.eyJ1IjoibmllbHNtb2VucyIsImEiOiJjanV3Z2w0c2gwNmdvNDRwZ3BvcGZtMGNwIn0.XhWcDDDcTVuFc3DkBPLTlg';
     const map = new window.mapboxgl.Map({
-      accessToken: 'pk.eyJ1IjoibmllbHNtb2VucyIsImEiOiJjanV3Z2w0c2gwNmdvNDRwZ3BvcGZtMGNwIn0.XhWcDDDcTVuFc3DkBPLTlg',
+      accessToken: accesstoken,
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [3.733333, 51.049999], // starting position [lng, lat]
@@ -106,6 +108,30 @@ class VisitorDashboard extends Component {
         trackUserLocation: true,
       }),
     );
+
+    await BusinessNames.getRegistered()
+      .then((response) => {
+        for (const [key, value] of Object.entries(response)) {
+          const mapboxClient = mapboxSdk({ accessToken: accesstoken });
+          mapboxClient.geocoding
+            .forwardGeocode({
+              query: `${value.location}, Ghent`,
+              autocomplete: false,
+              limit: 1,
+            })
+            .send()
+            // eslint-disable-next-line no-loop-func
+            .then((mapresponse) => {
+              if (mapresponse && mapresponse.body
+                && mapresponse.body.features
+                && mapresponse.body.features.length
+              ) {
+                const feature = mapresponse.body.features[0];
+                new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+              }
+            });
+        }
+      });
 
     // return the home container
     return homeContainer;
--- src/lib/BusinessNames.js
+++ src/lib/BusinessNames.js
@@ -4,6 +4,7 @@
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import { log } from 'handlebars';
 
 const BusinessNames = {
   //  Fetch  all the business data from the stad gent API
@@ -38,6 +39,37 @@ const BusinessNames = {
             // https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
             const res = apiResults.filter((item) => !filter.includes(item));
             console.log(res);
+            resolve(res);
+          });
+      });
+  }),
+
+  // Special thanks to Kobe for helping
+  getRegistered: async () => new Promise((resolve) => {
+    const url = 'https://data.stad.gent/api/records/1.0/search/?dataset=koop-lokaal-horeca&q=&rows=420&facet=postcode&facet=gemeente&refine.postcode=9000';
+    fetch(url)
+      .then((data) => data.json())
+      .then(async (data) => {
+        //  store only the business names and throw them in an array
+        const result = [];
+        for (const [, value] of Object.entries(data.records)) {
+          const businessInfo = {
+            name: value.fields.naam,
+            location: value.fields.adres,
+          };
+          result.push(businessInfo);
+        }
+        firebase.firestore().collection('BusinessRegistered').get()
+          .then((FireStoreRegisteredData) => {
+            const filter = [];
+            FireStoreRegisteredData.forEach((firestoreData) => {
+              filter.push(firestoreData.data());
+            });
+
+            // Check all registered businesses
+            // https://stackoverflow.com/questions/21987909/how-to-get-the-difference-between-two-arrays-of-objects-in-javascript
+            // eslint-disable-next-line max-len
+            const res = result.filter(({ name: id1 }) => filter.some(({ businessName: id2 }) => id2 === id1));
             resolve(res);
           });
       });
